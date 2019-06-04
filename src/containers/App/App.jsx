325f434495ee8c4ffb474744e26468edacc85e1d
ashfaqnisar <ashfaqnisar00@gmail.com>
2019-06-05 02:12:22 +0530

    15: Added the scss files and changed the style a little bit

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -4,6 +4,7 @@ import {Provider} from 'react-redux';
 import store from './store';
 import Router from './Router'
 import {BrowserRouter} from "react-router-dom";
+import '../../scss/index.scss';
 
 
 class App extends React.Component {
