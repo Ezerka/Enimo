142dd135fb66092f7f98cea9ba5bd085008de11f
ashfaqnisar <ashfaqnisar00@gmail.com>
2019-06-05 16:12:51 +0530

    22: Organized the files and changed the color of the loading bar

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -30,7 +30,7 @@ class App extends React.Component {
                         {!loaded && (<div className={`load${loading ? '' : ' loaded'}`}>
                             <div className="load__icon-wrap">
                                 <svg className="load__icon">
-                                    <path fill="#4ce1b6" d="M12,4V2A10,10 0 0,0 2,12H4A8,8 0 0,1 12,4Z"/>
+                                    <path fill="#ff4861" d="M12,4V2A10,10 0 0,0 2,12H4A8,8 0 0,1 12,4Z"/>
                                 </svg>
                             </div>
                         </div>)
