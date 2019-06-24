c3566066dcaecdaba79432b9b7d64904ad116eda
ashfaqnisar <ashfaqnisar00@gmail.com>
2019-06-24 11:25:58 +0530

    Updates

---
+++ precache-manifest.db52392b3ed3c7700d5becf0be83ab9d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e6e5c83a192eb17a25d5b496a922c64",
+    "url": "/enimo/index.html"
+  },
+  {
+    "revision": "87f6d10f97b69161680d",
+    "url": "/enimo/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "11e5d9f4a8034e7d6975",
+    "url": "/enimo/static/css/main.2f4d15ef.chunk.css"
+  },
+  {
+    "revision": "87f6d10f97b69161680d",
+    "url": "/enimo/static/js/2.d361704c.chunk.js"
+  },
+  {
+    "revision": "11e5d9f4a8034e7d6975",
+    "url": "/enimo/static/js/main.382e65d3.chunk.js"
+  },
+  {
+    "revision": "deee01fda53e396cf51b",
+    "url": "/enimo/static/js/runtime~main.e7a99bab.js"
+  },
+  {
+    "revision": "78d3e5b53c01186f648ee3ad49a3e79f",
+    "url": "/enimo/static/media/404.78d3e5b5.png"
+  },
+  {
+    "revision": "32900815f43e5001af46676ea9932913",
+    "url": "/enimo/static/media/logo.32900815.svg"
+  }
+]);
