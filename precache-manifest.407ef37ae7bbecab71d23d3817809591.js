0b2e0d55a6c29d0357340a1e3ad433806495718b
ashfaqnisar <ashfaqnisar00@gmail.com>
2019-06-24 16:29:46 +0530

    Updates

---
+++ precache-manifest.407ef37ae7bbecab71d23d3817809591.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3efd34c6199ad428825053ae41a19102",
+    "url": "/Enimo/index.html"
+  },
+  {
+    "revision": "6d82fa41842c55c96478",
+    "url": "/Enimo/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "b7e1fee99137a37dbf68",
+    "url": "/Enimo/static/css/main.d2ba04f3.chunk.css"
+  },
+  {
+    "revision": "6d82fa41842c55c96478",
+    "url": "/Enimo/static/js/2.92847c53.chunk.js"
+  },
+  {
+    "revision": "b7e1fee99137a37dbf68",
+    "url": "/Enimo/static/js/main.0916d99c.chunk.js"
+  },
+  {
+    "revision": "9adbf9b14a09ce98c36b",
+    "url": "/Enimo/static/js/runtime~main.fccfb23d.js"
+  },
+  {
+    "revision": "78d3e5b53c01186f648ee3ad49a3e79f",
+    "url": "/Enimo/static/media/404.78d3e5b5.png"
+  },
+  {
+    "revision": "32900815f43e5001af46676ea9932913",
+    "url": "/Enimo/static/media/logo.32900815.svg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/Enimo/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/Enimo/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/Enimo/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "66c03bd7e795b19e5e4f889064ce1e47",
+    "url": "/Enimo/static/media/weathericons-regular-webfont.66c03bd7.svg"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/Enimo/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  }
+]);
