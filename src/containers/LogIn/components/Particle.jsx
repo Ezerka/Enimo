f3f815722543734976d169536ad559a069e7b8c7
ashfaqnisar <ashfaqnisar00@gmail.com>
2019-06-25 23:59:17 +0530

    55: Added the particle js to the component, but it still needs some work to be responsive

---
+++ src/containers/LogIn/components/Particle.jsx
@@ -0,0 +1,130 @@
+import React from 'react'
+import Particles from 'react-particles-js'
+
+const ParticleOptions = {
+    "particles": {
+        "number": {
+            "value": 80,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#ffffff"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 1,
+                "color": "#ffffff"
+            },
+            "polygon": {
+                "nb_sides": 5
+            },
+        },
+        "opacity": {
+            "value": 0.4905098808642954,
+            "random": false,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 6,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "style": {
+        "width": "inherit",
+        "height": "inherit",
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "grab"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 278.011794439764,
+                "line_linked": {
+                    "opacity": 1
+                }
+            },
+            "bubble": {
+                "distance": 400,
+                "size": 40,
+                "duration": 2,
+                "opacity": 8,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 200,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true
+};
+
+const ParticleBackground = () => (
+    <div
+        style={{
+            position: "relative",
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            height: '100%',
+            width: '100%',
+        }}
+    >
+        <Particles params={ParticleOptions}/>
+    </div>
+);
+
+export default ParticleBackground
--- src/containers/LogIn/index.jsx
+++ src/containers/LogIn/index.jsx
@@ -1,22 +1,40 @@
 import React from 'react';
 import Form from './components/Form'
 import ShowResults from './components/Show.jsx'
+import ParticleBackground from './components/Particle'
+import {BrowserRouter} from "react-router-dom";
+
 
 const logo = `${process.env.PUBLIC_URL}/img/logo.svg`;
 
 const Login = () => (
-    <div className="login">
-        <div className="login__wrapper">
-            <div className="login__card">
-                <h3 className="login__title text-center">
-                    <img style={{align: "center", width: "50%",}} src={logo} alt="Logo"/>
-                    <span className="login__logo">
+    <BrowserRouter>
+        <div>
+            <ParticleBackground/>
+            <div style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0
+            }}>
+                <div className="login">
+                    <div className="login__wrapper">
+                        <div className="login__card">
+                            <h3 className="login__title text-center">
+                                <img style={{align: "center", width: "50%",}} src={logo}
+                                     alt="Logo"/>
+                                <span className="login__logo">
                         </span>
-                </h3>
-                <Form onSubmit = {ShowResults}/>
+                            </h3>
+                            <Form onSubmit={ShowResults}/>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
+    </BrowserRouter>
+
 );
 
 export default Login;
