4f05f2189ae4b89bb65014741c6e7964a5cda7a9
ashfaqnisar <ashfaqnisar00@gmail.com>
2019-06-27 11:49:57 +0530

    57: Remodified the login Page

--- src/containers/LogIn/index.jsx
+++ src/containers/LogIn/index.jsx
@@ -9,27 +9,17 @@ const logo = `${process.env.PUBLIC_URL}/img/logo.svg`;
 
 const Login = () => (
     <BrowserRouter>
-        <div>
-            <ParticleBackground/>
-            <div style={{
-                position: "absolute",
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0
-            }}>
-                <div className="login">
-                    <div className="login__wrapper">
-                        <div className="login__card">
-                            <h3 className="login__title text-center">
-                                <img style={{align: "center", width: "50%",}} src={logo}
-                                     alt="Logo"/>
-                                <span className="login__logo">
+        <ParticleBackground/>
+        <div className="login">
+            <div className="login__wrapper">
+                <div className="login__card">
+                    <h3 className="login__title text-center">
+                        <img style={{align: "center", width: "50%",}} src={logo}
+                             alt="Logo"/>
+                        <span className="login__logo">
                         </span>
-                            </h3>
-                            <Form onSubmit={ShowResults}/>
-                        </div>
-                    </div>
+                    </h3>
+                    <Form onSubmit={ShowResults}/>
                 </div>
             </div>
         </div>
