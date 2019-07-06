e58f707742a7235d12ccc8436934e03626dcbdad
ashfaqnisar <ashfaqnisar00@gmail.com>
2019-07-06 12:09:34 +0530

    88: Added the login form to the git.

---
+++ src/containers/Account/LogIn/components/LogInForm.jsx
@@ -0,0 +1,93 @@
+import React, {PureComponent} from 'react';
+import {Field, reduxForm} from 'redux-form';
+import EyeIcon from 'mdi-react/EyeIcon';
+import KeyVariantIcon from 'mdi-react/KeyVariantIcon';
+import AccountOutlineIcon from 'mdi-react/AccountOutlineIcon';
+import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import renderCheckBoxField from '../../../../shared/components/form/CheckBox';
+
+class LogInForm extends PureComponent {
+    static propTypes = {
+        handleSubmit: PropTypes.func.isRequired,
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showPassword: false,
+        };
+
+        this.showPassword = this.showPassword.bind(this);
+    }
+
+    showPassword(e) {
+        e.preventDefault();
+        this.setState({
+            showPassword: !this.state.showPassword,
+        });
+    }
+
+    render() {
+        const {handleSubmit} = this.props;
+
+        return (
+            <form className="form" onSubmit={handleSubmit}>
+                <div className="form__form-group">
+                    <span className="form__form-group-label">Username</span>
+                    <div className="form__form-group-field">
+                        <div className="form__form-group-icon">
+                            <AccountOutlineIcon/>
+                        </div>
+                        <Field
+                            name="username"
+                            component="input"
+                            type="text"
+                            placeholder="Name"
+                        />
+                    </div>
+                </div>
+                <div className="form__form-group">
+                    <span className="form__form-group-label">Password</span>
+                    <div className="form__form-group-field">
+                        <div className="form__form-group-icon">
+                            <KeyVariantIcon/>
+                        </div>
+                        <Field
+                            name="password"
+                            component="input"
+                            type={this.state.showPassword ? 'text' : 'password'}
+                            placeholder="Password"
+                        />
+                        <button
+                            className={`form__form-group-button${this.state.showPassword ? ' active' : ''}`}
+                            onClick={e => this.showPassword(e)}
+                        ><EyeIcon/>
+                        </button>
+                    </div>
+                </div>
+                <div className="form__form-group">
+                    <div className="form__form-group-field">
+                        <Field
+                            name="remember_me"
+                            component={renderCheckBoxField}
+                            label="Remember me"
+                        />
+                        <Link className="account__forgot-password" to="/home">Forgot the
+                            password?</Link>
+                    </div>
+                </div>
+                <div className="account__btns">
+                    <Link className="btn btn-secondary account__btn" to="/home">Sign In</Link>
+                    <Link className="btn btn-outline-secondary account__btn" to="/home">Create
+                        Account
+                    </Link>
+                </div>
+            </form>
+        );
+    }
+}
+
+export default reduxForm({
+    form: 'log_in_form', // a unique identifier for this form
+})(LogInForm);
